8fa6ae378ef7f79f7fde34fa4cd4c5053d18f7d6
Abdullah Al Mamun <mamuninfo@gmail.com>
2013-09-02 16:18:36 +0200

    update binding for cljs

--- config/clj-workflow.el
+++ config/clj-workflow.el
@@ -2,57 +2,54 @@
 (require 'nrepl )
 
 (defun dev-start ()
-    "Evaluate the current buffer's namespace form."
-      (interactive)
-      (message "Server is strarting...")
-      (nrepl-interactive-eval "(do
+  "Evaluate the current buffer's namespace form."
+  (interactive)
+  (message "Server is strarting...")
+  (nrepl-interactive-eval "(do
                                  (require 'dev)
                                  (dev/start)) " ))
 
 (defun dev-stop ()
-    "Evaluate the current buffer's namespace form."
-      (interactive)
-      (message "Server is stoping...." )
-      (nrepl-interactive-eval "(do (require 'dev)
+  "Evaluate the current buffer's namespace form."
+  (interactive)
+  (message "Server is stoping...." )
+  (nrepl-interactive-eval "(do (require 'dev)
                                    (dev/stop)) " ))
 
 
 (global-set-key (kbd "<f9>") 'dev-start )
-(global-set-key (kbd "<f8>") 'dev-stop )
-;(global-set-key (kbd "<f10>") 'cljs )
+(global-set-key (kbd "C-<f9>") 'dev-stop )
+                                        ;(global-set-key (kbd "<f10>") 'cljs )
                                         ;(global-set-key (kbd "<f11>") 'cljs-quit )
 
 (setq repl "clj")
 
-(defun repl-switch ()
+(defun cljs-repl ()
   (interactive)
-  (if (string-equal repl "cljs")
-      (progn
-	(message "please wait, repl will switch to clj-repl")
-	 (setq repl "clj")
- 
-        (nrepl-interactive-eval ":cljs/quit" )
-	(nrepl-set-ns "user")
-        (setq repl "clj")
-        )
-    (progn
-      (message "please wait, repl will switch to cljs-repl")
-      (setq repl "cljs")
-      (nrepl-interactive-eval "
+  (progn
+    (message "please wait, repl will switch to cljs-repl")
+    (setq repl "cljs")
+    (nrepl-interactive-eval "
 			      (do (require 'cljs.repl.browser)
 			      (cemerick.piggieback/cljs-repl
-                                :repl-env (doto	
+                                :repl-env (doto
 					    (cljs.repl.browser/repl-env :cport 9000)
 					    (cljs.repl/-setup))))" )
-      (nrepl-set-ns "cljs.user")
-     
-      )
-    )
-  )
+    (nrepl-set-ns "cljs.user")))
+
+(defun clj-repl ()
+  (interactive)
+  (progn
+    (message "please wait, repl will switch to clj-repl")
+    (setq repl "clj")
+    (nrepl-interactive-eval ":cljs/quit" )
+    (nrepl-set-ns "user")
+    (setq repl "clj")))
 
 (eval-after-load 'nrepl
-	   '(progn
-	      (global-set-key (kbd "<f11>") 'repl-switch )))
+  '(progn
+     (global-set-key (kbd "C-<f11>") 'cljs-repl )
+     (global-set-key (kbd "<f11>") 'clj-repl )))
 
 
-;(global-set-key (kbd "<f11>") 'repl-switch )
+                                        ;(global-set-key (kbd "<f11>") 'repl-switch )
